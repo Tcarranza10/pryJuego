aa354302c5ff17ec51b1496eed40d24a4739a8f7
Tomás Carranza <tomi_carranza@hotmail.com>
2024-04-09 23:48:22 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace pryJuego
+{
+    public partial class Form1 : Form
+    {
+        //Declaracion de variables globales
+        clsNave objNaveJugador;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            objNaveJugador = new clsNave();
+
+            objNaveJugador.crearJugador();
+
+            objNaveJugador.imgNave.Location = new Point(200,200) ;
+            
+            Controls.Add(objNaveJugador.imgNave);
+            //MessageBox.Show(objNaveJugador.nombre);
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Right)
+            {
+                objNaveJugador.imgNave.Location = new Point(
+                    objNaveJugador.imgNave.Location.X + 5,
+                    objNaveJugador.imgNave.Location.Y);
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                objNaveJugador.imgNave.Location = new Point(
+                    objNaveJugador.imgNave.Location.X - 5,
+                    objNaveJugador.imgNave.Location.Y);
+            }
+        }
+    }
+}
---
+++ clsNave.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryJuego
+{
+    internal class clsNave
+    {
+        //caracteristicas 
+        public int vida;
+        public string nombre;
+        public int puntosDaño;
+        public PictureBox imgNave;
+
+        public void crearJugador()
+        {
+            vida = 100;
+            nombre = "jugador1";
+            puntosDaño = 1;
+            imgNave = new PictureBox();
+            imgNave.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgNave.ImageLocation = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQInlJtDw4AOVWmJ8EBzQoUBE5CIXDPNDyXeA&s";
+        }
+
+        public void crearEnemigo()
+        {
+            //enemigo 1
+            vida = 25;
+            nombre = "enemigo1";
+            puntosDaño = 2;
+            imgNave = new PictureBox();
+            imgNave.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgNave.ImageLocation = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX9AAx6-LDfy0xe8B1-QzMRkp19ZzwlaESKA&s";
+
+            //enemigo 2
+            vida = 25;
+            nombre = "enemigo2";
+            puntosDaño = 2;
+            imgNave=new PictureBox();
+            imgNave.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgNave.ImageLocation = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbYhg6FfNcC6Kd04YZn1o44Mg4kCXLRvQkbw&s";
+        }
+
+    }
+}
